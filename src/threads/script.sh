ed056896c4804b9967e2d23191401fa5d7822928
Qiang <qzeng2490@yahoo.com>
2022-03-23 00:52:34 +0800

    bootloader调试环境设置

--- src/threads/script.sh
+++ src/threads/script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 make
 cd build || exit
-killall qemu-system-i386 # 杀掉上一次打开的QEMU。Clion不能在停止Debug的时候关闭QEMU。
-
-CMD="run alarm-single" # 你要运行的指令。
-nohup bash -c "DISPLAY=window ../../utils/pintos --qemu --gdb -- $CMD > pintos.log" &
-echo "Done!"
+#killall qemu-system-i386 # 杀掉上一次打开的QEMU。Clion不能在停止Debug的时候关闭QEMU。
+#
+#CMD="run alarm-single" # 你要运行的指令。
+#nohup bash -c "DISPLAY=window ../../utils/pintos --qemu --gdb -- $CMD > pintos.log" &
+#echo "Done!"
